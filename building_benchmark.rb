142c75f14c68aa0ba3fbfc5397cc7692ad1a1298
Eneroth3 <eneroth3@gmail.com>
2016-06-09 21:51:40 +0200

    (Hpefully) managed to get fixed spelling into this branch
    Without overriding building.rb as happened when I tried to cherry pick the other branch.

---
+++ building_benchmark.rb
@@ -0,0 +1,12 @@
+EneBuildings::Template.require_all
+building          = EneBuildings::Building.new
+building.template = EneBuildings::Template.get_from_id "ene_landshovdingehus_reveterat"
+building.path     = [ORIGIN, Geom::Point3d.new(45.m, 0, 0)]
+
+start = Time.now
+
+Sketchup.active_model.start_operation "Draw Building", true
+building.draw
+Sketchup.active_model.commit_operation
+
+UI.messagebox "Drawn in #{Time.now - start}s."
