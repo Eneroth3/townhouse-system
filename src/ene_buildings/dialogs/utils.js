f2c3fe8dc7df9562adde19d3b5cd4310d635d4e2
Eneroth3 <eneroth3@gmail.com>
2026-03-19 08:06:27 +0100

    Fix broken html escaping

--- src/ene_buildings/dialogs/utils.js
+++ src/ene_buildings/dialogs/utils.js
@@ -1,14 +1,10 @@
-function escape_html(text){
-  //if (typeof text !== 'string' && !(text instanceof String)) {
-  //  throw('"' + text + '" is not a string.')
-  //}
-  
+function escape_html(text){ 
   text = text.toString();
   
   return text.replace(/&/g, "&amp;"
       ).replace(/</g, "&lt;"
       ).replace(/>/g, "&gt;"
       ).replace(/"/g, "&quot;"
-      ).replace(/'/g, "&#x27"
+      ).replace(/'/g, "&#x27;"
     )
 }
